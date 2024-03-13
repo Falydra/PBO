d717cad2296066e452ffc6175f270bfc8fe3d353
Aly <100826322+Falydra@users.noreply.github.com>
2024-03-13 17:25:05 +0700

    initial commit

---
+++ PBO/Praktikum_4/TugasModul/src/org/main/MSegitiga.java
@@ -0,0 +1,17 @@
+/**
+ * File: MSegitiga.java
+ * Nama: Daffa Aly Meganendra
+ * NIM : 24060122140140
+ */
+
+package org.main;
+
+import org.bangundatar.*;
+
+public class MSegitiga{
+    public static void main(String[] args) {
+        Segitiga segitiga = new Segitiga(10, 10);
+
+        System.out.println("Luas: " + segitiga.hitungLuas());
+    }
+}
